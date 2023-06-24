5dac5214181880d9a37a4a4abd9925d1cae51eb1
Guilherme Bley <guilhermecbley@gmail.com>
2023-06-23 23:56:02 -0300

    set repositories

---
+++ Demo.RabbitMq.GitHubProfile/Repositories/GitRepoRepository.cs
@@ -0,0 +1,35 @@
+﻿using Demo.RabbitMq.GitHubProfile.Model;
+using System.Collections.Concurrent;
+
+namespace Demo.RabbitMq.GitHubProfile.Repositories;
+
+internal interface IGitRepoRepository
+{
+    Task AddAsync(string name, GitRepositoryModel[] gitRepositoryModels, CancellationToken cancellationToken = default);
+    Task RemoveByNameAsync(string name, CancellationToken cancellationToken = default);
+    Task<IEnumerable<GitRepositoryModel>> GetAllByNameAsync(string name, CancellationToken cancellationToken = default);
+    Task<IEnumerable<string>> GetRepositoriesNamesAsync(CancellationToken cancellationToken = default);
+}
+
+internal class GitRepoRepository : IGitRepoRepository
+{
+    private static readonly ConcurrentDictionary<string, IEnumerable<GitRepositoryModel>> _poolGitRepo = new();
+
+    public async Task AddAsync(string name, GitRepositoryModel[] gitRepositoryModels, CancellationToken cancellationToken = default)
+        => await Task.FromResult( 
+            _poolGitRepo.AddOrUpdate(name, gitRepositoryModels, (name, current) => gitRepositoryModels));
+
+    public async Task<IEnumerable<GitRepositoryModel>> GetAllByNameAsync(string name, CancellationToken cancellationToken = default)
+        => await Task.FromResult(
+            _poolGitRepo.GetValueOrDefault(name) ?? Enumerable.Empty<GitRepositoryModel>());
+
+    public async Task<IEnumerable<string>> GetRepositoriesNamesAsync(CancellationToken cancellationToken = default)
+        => await Task.FromResult(
+            _poolGitRepo.Keys    
+        );
+
+    public async Task RemoveByNameAsync(string name, CancellationToken cancellationToken = default)
+        => await Task.FromResult(
+            _poolGitRepo.TryRemove(name, out IEnumerable<GitRepositoryModel>? _)    
+        );
+}
