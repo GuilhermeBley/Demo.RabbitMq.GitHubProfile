3cc58eb21c916fb65d5732832ef2deec0f042680
Guilherme Bley <guilhermecbley@gmail.com>
2023-06-23 23:39:13 -0300

    add git interface

---
+++ Demo.RabbitMq.GitHubProfile/Api/GithubApi.cs
@@ -0,0 +1,16 @@
+﻿using Demo.RabbitMq.GitHubProfile.Model;
+
+namespace Demo.RabbitMq.GitHubProfile.Api;
+
+internal interface IGithubApi
+{
+    Task<IEnumerable<GitRepositoryModel>> GetAllRepositoriesAsync(string name, CancellationToken cancellationToken = default);
+}
+
+internal class GithubApi : IGithubApi
+{
+    public Task<IEnumerable<GitRepositoryModel>> GetAllRepositoriesAsync(string name, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Demo.RabbitMq.GitHubProfile/Model/GitRepositoryModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo.RabbitMq.GitHubProfile.Model;
+
+public class GitRepositoryModel
+{
+    public string Url { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string[] Topics { get; set; } = new string[0];
+    public DateTime CreateAt { get; set; }
+}
