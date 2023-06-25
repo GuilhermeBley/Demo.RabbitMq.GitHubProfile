f3ef2f85b0f5b802b3b01c34ddddce5a6f8b0225
Guilherme Bley <guilhermecbley@gmail.com>
2023-06-24 23:40:26 -0300

    add log

--- Demo.RabbitMq.GitHubProfile/Workers/QueryGitRepoWorker.cs
+++ Demo.RabbitMq.GitHubProfile/Workers/QueryGitRepoWorker.cs
@@ -1,13 +1,25 @@
-﻿namespace Demo.RabbitMq.GitHubProfile.Workers
+﻿using Demo.RabbitMq.GitHubProfile.Repositories;
+
+namespace Demo.RabbitMq.GitHubProfile.Workers;
+
+public class QueryGitRepoWorker : BackgroundService
 {
-    public class QueryGitRepoWorker : BackgroundService
+    private IGitRepoRequestsRepository _gitRepoRequestsRepository;
+    private readonly ILogger<QueryGitRepoWorker> _log;
+
+    public QueryGitRepoWorker(ILogger<QueryGitRepoWorker> log, IGitRepoRequestsRepository gitRepoRequestsRepository)
     {
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        _gitRepoRequestsRepository = gitRepoRequestsRepository;
+        _log = log;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
         {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                await Task.Delay(1000, stoppingToken);
-            }
+            _log.LogInformation($"QueryGitRepoWorker run at: '{DateTime.UtcNow.ToString("yyyy/MM/dd HH:mm:ss")}' (UTC)");
+
+            await Task.Delay(1000, stoppingToken);
         }
     }
 }
