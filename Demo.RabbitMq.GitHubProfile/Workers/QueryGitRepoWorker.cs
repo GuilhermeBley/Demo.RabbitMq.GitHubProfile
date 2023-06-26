f5480c3e1e4071083c30d472fc26e823f7e0f46e
GuilhermeBley <guilhermecbley@gmail.com>
2023-06-25 21:53:34 -0300

    worker

--- Demo.RabbitMq.GitHubProfile/Workers/QueryGitRepoWorker.cs
+++ Demo.RabbitMq.GitHubProfile/Workers/QueryGitRepoWorker.cs
@@ -1,15 +1,21 @@
-﻿using Demo.RabbitMq.GitHubProfile.Repositories;
+﻿using Demo.RabbitMq.GitHubProfile.Api;
+using Demo.RabbitMq.GitHubProfile.Model;
+using Demo.RabbitMq.GitHubProfile.Repositories;
 
 namespace Demo.RabbitMq.GitHubProfile.Workers;
 
 public class QueryGitRepoWorker : BackgroundService
 {
     private IGitRepoRequestsRepository _gitRepoRequestsRepository;
+    private readonly IGitApi _gitApi;
+    private readonly GitRepoRepository _gitRepoRepository;
     private readonly ILogger<QueryGitRepoWorker> _log;
 
-    public QueryGitRepoWorker(ILogger<QueryGitRepoWorker> log)
+    public QueryGitRepoWorker(ILogger<QueryGitRepoWorker> log, IGitApi gitApi)
     {
         _gitRepoRequestsRepository = new GitRepoRequestsRepository();
+        _gitApi = gitApi;
+        _gitRepoRepository = new GitRepoRepository();
         _log = log;
     }
 
@@ -19,7 +25,25 @@ public class QueryGitRepoWorker : BackgroundService
         {
             _log.LogInformation($"QueryGitRepoWorker run at: '{DateTime.UtcNow.ToString("yyyy/MM/dd HH:mm:ss")}' (UTC)");
 
+            
+
             await Task.Delay(1000, stoppingToken);
         }
+    }
+
+    private async Task TryGetAndSetGitRepositoryByRequest(GitRepositoryRequestModel model, CancellationToken cancellationToken = default)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var repositoriesFound = await _gitApi.GetAllRepositoriesAsync(model.RequestedName, cancellationToken: cancellationToken);
+
+        if (repositoriesFound is null)
+            return;
+
+        await _gitRepoRepository.AddAsync(
+            model.RequestedName,
+            repositoriesFound.ToArray(),
+            cancellationToken: cancellationToken
+        );
     }
 }
