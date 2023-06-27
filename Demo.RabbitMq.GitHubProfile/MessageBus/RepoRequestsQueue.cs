c29f19609ce92c6ee0b26507640d18ea56ddf2d5
GuilhermeBley <guilhermecbley@gmail.com>
2023-06-26 23:45:07 -0300

    add in queue

--- Demo.RabbitMq.GitHubProfile/MessageBus/RepoRequestsQueue.cs
+++ Demo.RabbitMq.GitHubProfile/MessageBus/RepoRequestsQueue.cs
@@ -1,22 +1,40 @@
 using Demo.RabbitMq.GitHubProfile.Model;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace Demo.RabbitMq.GitHubProfile.MessageBus;
 
-public interface IRepoRequestsQueue : IDisposable, IAsyncDisposable
+public interface IRepoRequestsQueue : IDisposable
 {
     Task<GitRepositoryRequestModel?> TryGetNext();
+    Task AddAsync(GitRepositoryRequestModel model);
 }
 
 public class RepoRequestsQueue : IRepoRequestsQueue
 {
-    public void Dispose()
+    private readonly IConnection _connection;
+    private readonly IModel _channel;
+
+    public RepoRequestsQueue(string connectionString)
+    {
+        var factory = new ConnectionFactory()
+        {
+            Uri = new Uri(connectionString)
+        };
+
+        _connection = factory.CreateConnection();
+        _channel = _connection.CreateModel();
+    }
+
+    public Task AddAsync(GitRepositoryRequestModel model)
     {
         throw new NotImplementedException();
     }
 
-    public ValueTask DisposeAsync()
+    public void Dispose()
     {
-        throw new NotImplementedException();
+        _connection.Dispose();
+        _channel.Dispose();
     }
 
     public Task<GitRepositoryRequestModel?> TryGetNext()
