234bd23ce389972d414db032c660e36e5dc1cebb
GuilhermeBley <guilhermecbley@gmail.com>
2023-06-27 00:11:31 -0300

    RepoRequestsQueue implementation

--- Demo.RabbitMq.GitHubProfile/MessageBus/RepoRequestsQueue.cs
+++ Demo.RabbitMq.GitHubProfile/MessageBus/RepoRequestsQueue.cs
@@ -1,3 +1,5 @@
+using System.Text;
+using System.Threading.Channels;
 using Demo.RabbitMq.GitHubProfile.Model;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
@@ -6,12 +8,14 @@ namespace Demo.RabbitMq.GitHubProfile.MessageBus;
 
 public interface IRepoRequestsQueue : IDisposable
 {
-    Task<GitRepositoryRequestModel?> TryGetNext();
+    void OnGetData(Action<GitRepositoryRequestModel> onGetData);
     Task AddAsync(GitRepositoryRequestModel model);
 }
 
 public class RepoRequestsQueue : IRepoRequestsQueue
 {
+    private const string QUEUE_NAME = "repo_requests";
+
     private readonly IConnection _connection;
     private readonly IModel _channel;
 
@@ -26,9 +30,22 @@ public class RepoRequestsQueue : IRepoRequestsQueue
         _channel = _connection.CreateModel();
     }
 
-    public Task AddAsync(GitRepositoryRequestModel model)
+    public async Task AddAsync(GitRepositoryRequestModel model)
     {
-        throw new NotImplementedException();
+        TryDeclareQueue();
+
+        var jsonText = System.Text.Json.JsonSerializer.Serialize(model);
+
+        var bytes = Encoding.UTF8.GetBytes(jsonText);
+
+        _channel.BasicPublish(
+            exchange: string.Empty,
+            routingKey: QUEUE_NAME,
+            basicProperties: null,
+            body: bytes
+        );
+
+        await Task.CompletedTask;
     }
 
     public void Dispose()
@@ -37,8 +54,37 @@ public class RepoRequestsQueue : IRepoRequestsQueue
         _channel.Dispose();
     }
 
-    public Task<GitRepositoryRequestModel?> TryGetNext()
+    public void OnGetData(Action<GitRepositoryRequestModel> onGetData)
     {
-        throw new NotImplementedException();
+        TryDeclareQueue();
+
+        var consumer = new EventingBasicConsumer(_channel);
+
+        consumer.Received += (model, ea) =>
+        {
+            var jsonText = Encoding.UTF8.GetString(
+                ea.Body.ToArray());
+
+            var obj = System.Text.Json.JsonSerializer.Deserialize<GitRepositoryRequestModel>(jsonText);
+
+            if (obj is null)
+                return;
+
+            onGetData.Invoke(obj);
+        };
+
+        _channel.BasicConsume(
+            queue: QUEUE_NAME,
+            autoAck: true,
+            consumer: consumer
+        );
     }
+
+    private void TryDeclareQueue()
+        => _channel.QueueDeclare(
+            queue: QUEUE_NAME,
+            durable: false, 
+            exclusive: false, 
+            autoDelete: false
+        );
 }
